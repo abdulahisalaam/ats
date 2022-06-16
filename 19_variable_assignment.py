62ed156001cd811a03cef52be23a283ac0c9c8d3
Abdullahi Kolawole Salaam <asalaam@afexnigeria.com>
2022-06-16 10:26:11 +0100

    Quadratic Task

---
+++ 19_variable_assignment.py
@@ -0,0 +1,18 @@
+# def profile(firstname, lastname, email, phone_number):
+#     print("First name {}. last name is {}".format(firstname,))
+
+# def find_average(num1, num2, num3, num4, num5):
+#     avg=(num1+num2+num3+num4+num5)/5
+#     print(f"the average is : {avg} ")
+
+# def func_type(num:int,word:str,height:float,record:list):
+#     print(f"the type of {num} is {type(num)}")
+#     print(f"the type of {word} is {type(word)}")
+#     print(f"the type of {height} is {type(height)}")
+#     print(f"the type of {record} is {type(record)}")
+
+# func_type(3, 'stead', 4.5, [1,2,3,5])
+
+
+
+
---
+++ 20_quadratic_program.py
@@ -0,0 +1,9 @@
+def find_quadratic_equation(a:int,b:int,c:int):
+    product_of_ac = a*c
+    factors =[i for i in range(-abs(product_of_ac),abs(product_of_ac)+1) if i != 0 and product_of_ac % i == 0]
+
+    final_factors= [factor for factor in factors for i in range(len(factors)) if factor + factors[i] == b and factor * factors[i]== c]
+
+    return ' and '.join(str(x) for x in final_factors)
+
+print(find_quadratic_equation(1,8,12))
